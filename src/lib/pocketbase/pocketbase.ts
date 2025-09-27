91bad8e0659adb9fe6c8e392356303abb3673976
Muhammad Afif Ramadhan <ramadhanafif@gmail.com>
2025-09-27 03:58:59 +0200

    build: remove pocketbase

--- src/lib/pocketbase/pocketbase.ts
+++
@@ -1,8 +0,0 @@
-import PocketBase from 'pocketbase';
-import { formatRFC3339 } from 'date-fns';
-
-// TODO: make pocketbase url configurable
-const pb = new PocketBase('http://127.0.0.1:8090');
-
-const pbTimeFormat = formatRFC3339;
-export { pb, pbTimeFormat };
